c7811031c53e0bcc20ee4594f2ee32882007ac7e
Luiz Felipe Cavalcante <luizf.a.cavalcante@gmail.com>
2025-04-30 23:12:59 +0000

    lista 09 completa

--- Lista09/exercicio7.c
+++ Lista09/exercicio7.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>  
 #include <time.h>  
+#define MAX 100
 
-void preencherMatriz(int *matriz, int n){
+void somaDiagonalPrincipal(int matriz[][MAX], int n){
+    int soma = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == j){
+                soma += matriz[i][j];
+            }
+        }
+    }
+    printf("%d", soma);
+}
+
+void somaAbaixoDiagonal(int matriz[][MAX], int n){
+    int soma = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i > j){
+                soma += matriz[i][j];
+            }
+        }
+    }
+    printf("%d", soma);
+}
+
+void preencherMatriz(int matriz[][MAX], int n){
     
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -15,12 +40,13 @@ void preencherMatriz(int *matriz, int n){
 int main(){
     srand(time(NULL));
 
-    int matriz[100];
+    int matriz[MAX][MAX];
     int n;
     printf("Digite o valor de n: ");
-    scanf("%d", n);
+    scanf("%d", &n);
 
     preencherMatriz(matriz, n);
-
+    somaDiagonalPrincipal(matriz, n);
+    somaAbaixoDiagonal(matriz, n);
     return 0;
 }
