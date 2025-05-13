aee44b59a059698ffbc51bb1b6e4bd3e1c35c89f
Luiz Felipe Cavalcante <luizf.a.cavalcante@gmail.com>
2025-05-13 13:54:12 +0000

    verdeIncompleto

--- Verde/contagemMaiusculo.c
+++ Verde/contagemMaiusculo.c
@@ -1,14 +1,19 @@
 #include <string.h>
+#include <ctype.h>
 
 int main(){
 char str[100];
 int n = 0;
-scanf("%[^\n]", str);
 do{
+scanf("%[^\n]", str);
 int len = strlen(str);
 for(int i = 0; i < len; i++){
-    printf("%C", str[i]);
+    if (isupper(str[i])) {
+        n ++;
+    }
 }
+printf("%d \n", n);
+n = 0;
 }while(str != "FIM");
 return 0;
 }
